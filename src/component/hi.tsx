91283da007cc45af83a763981ca0679a5113cd4d
PS/2020/203 - NUSDHA M.U <nusdham-ps20203@kln.ac.lk>
2024-12-30 11:19:48 +0530

    React tutorial

--- src/component/hi.tsx
+++ src/component/hi.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 interface HiProps {
-    children: React.ReactNode; //Accept any valid React node
+    children: React.ReactNode;
+     // ReactNode allows passing HTML or other components as children
 }
 
 const Hi: React.FC<HiProps> = ({ children }) => {
+    const handleAlertDismiss = () => {
+        alert("You clicked the button! Dismissed successfully.");
+    };
     return (
-        <div>
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
             {children}
+            <button 
+            type="button" className="btn-close" data-bs-dismiss="alert" aria-label="close" onClick={handleAlertDismiss}>
+            </button>
         </div>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+//export default defineConfig({
+  //plugins: [react()],
+//})
+
+export default {
+  server: {
+  port: 3000, // Change to the desired port
+  },//rest
+ };
